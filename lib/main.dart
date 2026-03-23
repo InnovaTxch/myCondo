01d715aba6cbc0c0a57834edf92988339befdb30
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-03-23 17:04:59 +0800

    feat: add empty folder placeholder to follow project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'screens/splash_screen.dart';
-import 'screens/role_selection.dart';
-import 'screens/login_screen.dart';
-import 'screens/signup_screen.dart';
-import 'screens/dashboard.dart';
+import 'app.dart';
 
 void main() {
   runApp(MyCondoApp());
 }
 
-class MyCondoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'myCondo',
-      debugShowCheckedModeBanner: false,
-
-      initialRoute: '/',
-
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/role': (context) => RoleSelection(),
-        '/login': (context) => LoginScreen(),
-        '/signup': (context) => SignupScreen(),
-        '/dashboard': (context) => Dashboard(),
-      },
-    );
-  }
-}
