9b33ab11ace8be7f9aaa812fa73c4e390dc07418
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-03-27 13:01:02 +0800

    feat: add create_bill_page

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,7 @@ import 'package:mycondo/features/auth/pages/role_selection.dart';
 import 'package:mycondo/features/auth/pages/login_screen.dart';
 import 'package:mycondo/features/auth/pages/signup_screen.dart';
 
+import 'package:mycondo/features/manager/pages/create_bill_page.dart';
 
 class MyCondoApp extends StatelessWidget {
   const MyCondoApp({super.key});
@@ -24,6 +25,7 @@ class MyCondoApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/signup': (context) => SignupScreen(),
         '/dashboard': (context) => Dashboard(),
+        '/add-bill': (context) => CreateBillPage()
       },
     );
   }
---
+++ lib/data/models/unit.dart
@@ -0,0 +1,7 @@
+import 'resident.dart';
+
+class Unit {
+  final String name;
+  final List<Resident> members;
+  Unit({required this.name, required this.members});
+}
