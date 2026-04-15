c07752e248b638a48c05d4ddfaba6a78a445993c
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-04-15 14:59:26 +0800

    refactor: change unit to unitName for better readability

--- lib/data/models/resident.dart
+++ lib/data/models/resident.dart
@@ -1,6 +1,10 @@
 class Resident {
   final String id;
   final String name;
-  final String unit;
-  Resident({required this.id, required this.name, required this.unit});
+  final String unitName;
+  Resident({
+    required this.id, 
+    required this.name,
+    required this.unitName
+  });
 }
